56806fd85b4aa9e08f776377bf2ae68cac102ff0
faba11 <traorefaba15@gmail.com>
2024-07-05 00:23:41 +0100

    some updates

--- projects/js/rockScissorPaper.js
+++ projects/js/rockScissorPaper.js
@@ -2,6 +2,18 @@
 let game = ["rock", "paper", "scissors"];
 
 
+// scores 
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+
+// Initialize the scores in the DOM
+let winScore = document.querySelector('.win').innerHTML = score.wins;
+let loseScore = document.querySelector('.lose').innerHTML = score.losses;
+let drawScore = document.querySelector('.draw').innerHTML = score.ties;
+
 // get computer move
 function getComputerMove() {
     const computerMove = Math.floor(Math.random() * game.length);
@@ -27,7 +39,8 @@ function playGame(playerMove) {
     if (computerMove === playerMove) {
         resultElement.innerHTML = "Draw!";
         resultElement.style.color = "orange";
-        document.querySelector('.draw').innerHTML++;
+
+        document.querySelector('.draw').innerHTML = score.ties++;
 
         // check if player won
     } else if (
@@ -37,20 +50,36 @@ function playGame(playerMove) {
     ) {
         resultElement.innerHTML = "you win!";
         resultElement.style.color = "green";
-        document.querySelector('.win').innerHTML++;
+
+
+        document.querySelector('.win').innerHTML = score.wins++;
 
         // check if player lost
     } else {
         resultElement.innerHTML = "you lose!";
         resultElement.style.color = "red";
-        document.querySelector('.lose').innerHTML++;
+
+        document.querySelector('.lose').innerHTML = score.losses++;
     }
+    // store score in local storage
+    localStorage.setItem('score', JSON.stringify(score));
 }
 
+
+// reset scores function
 function reset() {
-    document.querySelector('.win').innerHTML = 0;
-    document.querySelector('.lose').innerHTML = 0;
-    document.querySelector('.draw').innerHTML = 0;
+
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    }
+    document.querySelector('.win').innerHTML = score.wins;
+    document.querySelector('.lose').innerHTML = score.losses;
+    document.querySelector('.draw').innerHTML = score.ties;
+
+    // remove score from local storage
+    localStorage.removeItem('score');
 }
 
 playGame();
