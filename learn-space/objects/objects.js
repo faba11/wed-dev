6b1557180552275ba4903e8857f9f6f61b2ae6d2
faba11 <traorefaba15@gmail.com>
2024-07-06 11:28:52 +0100

    some update

--- learn-space/objects/objects.js
+++ learn-space/objects/objects.js
@@ -1,55 +1,55 @@
-// 'strict';
-// const product = {
-//     name: 'socks',
-//     price: 1090,
-//     isNewProuct: true
-// }
+'strict';
+const product = {
+    name: 'socks',
+    price: 1090,
+    isNewProuct: true
+}
 
-// // change the object's name
-// product.name = 'shoes'
-// product['color'] = 'red';
-// product.lenght = 32
-// product.width = 34
+// change the object's name
+product.name = 'shoes'
+product['color'] = 'red';
+product.lenght = 32
+product.width = 34
 
-// delete product.isNewProuct
+delete product.isNewProuct
 
-// // set name propety to objects
-
-
-// // supersimple.dev/projects/objects
-// console.log(product)
+// set name propety to objects
 
 
-// // product 2
-// let product2 = {
-//     name: 'shirt',
-//     price: 580,
-//     description: 'nice shirt',
-//     'delivery time': '3 days',
-//     rating: {
-//         stars: 4.5,
-//         count: 150
-//     },
-
-//     // function in the object
-//     ratingFunc: function rateFunc() {
-//         console.log(`${this.name} has ${this.rating.count} rating`)
-//     }
-// }
-
-// console.log(product2['delivery time'])
-// console.log(product2.rating.count);
-// product2.ratingFunc();
+// supersimple.dev/projects/objects
+console.log(product)
 
 
-// // convert javascript object into JSON
-// let jsonString = JSON.stringify(product2);
-// console.log(jsonString);
+// product 2
+let product2 = {
+    name: 'shirt',
+    price: 580,
+    description: 'nice shirt',
+    'delivery time': '3 days',
+    rating: {
+        stars: 4.5,
+        count: 150
+    },
+
+    // function in the object
+    ratingFunc: function rateFunc() {
+        console.log(`${this.name} has ${this.rating.count} rating`)
+    }
+}
+
+console.log(product2['delivery time'])
+console.log(product2.rating.count);
+product2.ratingFunc();
 
 
-// // convert json into javascrip object
-// let object = JSON.parse(jsonString);
-// console.log(object);
+// convert javascript object into JSON
+let jsonString = JSON.stringify(product2);
+console.log(jsonString);
+
+
+// convert json into javascrip object
+let object = JSON.parse(jsonString);
+console.log(object);
 
 
 
